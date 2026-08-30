807b7289ae1d7d23b227610e5ec2581aae15150e
Eripto <ryujigamer704@gmail.com>
2026-08-30 16:40:13 -0700

    Add iOS 12 arm64 build artifact

--- LiquidAssPrefs/LGPrefsLiquidSlider.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-extern void * const kLGPrefsSliderSegmentedKey;
-extern void * const kLGPrefsSliderSegmentCountKey;
-extern void * const kLGPrefsSliderSegmentCentersKey;
-extern void * const kLGPrefsSliderEndpointCentersKey;
-
-@interface LGPrefsLiquidSlider : UISlider
-@end
--- LiquidAssPrefs/LGPrefsLiquidSwitch.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface LGPrefsLiquidSwitch : UISwitch
-- (void)lg_beginExternalPress;
-- (void)lg_endExternalPressForToggle;
-- (void)lg_cancelExternalPress;
-@end
